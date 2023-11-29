ff71ca4197f7ab9a14d163a14fccd2270644d16b
Maria Luisa <100421466+Maria-Luisa-garcia@users.noreply.github.com>
2023-11-29 13:56:55 +0000

    💎Elementos PWA Algunos complementos de Capacitor, incluida la API de cámara, proporcionan la funcionalidad basada en la web y la interfaz de usuario a través de la biblioteca Ionic

--- photo-gallery/src/main.ts
+++ photo-gallery/src/main.ts
@@ -1,3 +1,7 @@
+// Above the createApp() line
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+defineCustomElements(window);
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
@@ -26,7 +30,7 @@ import './theme/variables.css';
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
-  
+
 router.isReady().then(() => {
   app.mount('#app');
 });

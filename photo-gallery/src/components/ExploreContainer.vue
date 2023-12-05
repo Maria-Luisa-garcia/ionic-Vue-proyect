53b798c1a1713addd0329b5a01ca49084d47976b
Maria Luisa <100421466+Maria-Luisa-garcia@users.noreply.github.com>
2023-12-05 21:29:15 +0000

    ✅ Aplicacion en ionic  funcionando

--- photo-gallery/src/components/ExploreContainer.vue
+++ photo-gallery/src/components/ExploreContainer.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="container">
     <strong>{{ name }}</strong>
-    <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    <p>
+      Explore
+      <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a>
+    </p>
   </div>
 </template>
 
-<script lang="ts">
-export default {
-  name: 'ExploreContainer',
-  props: {
-    name: String
-  }
-}
+<script setup lang="ts">
+defineProps({
+  name: String,
+});
 </script>
 
 <style scoped>
@@ -23,16 +23,19 @@ export default {
   top: 50%;
   transform: translateY(-50%);
 }
+
 #container strong {
   font-size: 20px;
   line-height: 26px;
 }
+
 #container p {
   font-size: 16px;
   line-height: 22px;
   color: #8c8c8c;
   margin: 0;
 }
+
 #container a {
   text-decoration: none;
 }
--- photo-gallery/tests/e2e/specs/tes.ts
+++ photo-gallery/tests/e2e/specs/tes.ts
@@ -1,7 +1,8 @@
+// https://docs.cypress.io/api/introduction/api.html
 
 describe('My First Test', () => {
   it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
-  })
-})
+    cy.visit('/');
+    cy.contains('ion-content', 'Tab 1 page');
+  });
+});

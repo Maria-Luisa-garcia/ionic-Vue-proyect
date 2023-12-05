9d1ee21d9dc70819602feda0abe0fa9e8d51981f
Maria Luisa <100421466+Maria-Luisa-garcia@users.noreply.github.com>
2023-12-05 17:22:59 +0000

    📸 Tomar fotos con la cámara

--- photo-gallery/src/views/Tab1Page.vue
+++ photo-gallery/src/views/Tab1Page.vue
@@ -16,8 +16,13 @@
     </ion-content>
   </ion-page>
 </template>
-
-<script setup lang="ts">
+<script>
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+export default  {
+  name: 'Tab1',
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+}
+
+
 </script>
